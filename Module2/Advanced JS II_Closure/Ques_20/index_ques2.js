dbd0e27b8a1773d4b7ee09c2edb3b6a8bb919fd5
SowmisHub <vemulasowmika123@gmail.com>
2025-11-26 21:59:26 +0530

    ques2

---
+++ Module2/Advanced JS II_Closure/Ques_20/index_ques2.js
@@ -0,0 +1,28 @@
+function createBankAccount() {
+  let balance = 0; 
+  return {
+    deposit(amount) {
+      balance += amount;
+      console.log("Deposited:", amount);
+    },
+
+    withdraw(amount) {
+      if (amount > balance) {
+        console.log("Insufficient balance");
+      } else {
+        balance -= amount;
+        console.log("Withdrawn:", amount);
+      }
+    },
+
+    checkBalance() {
+      console.log("Current Balance:", balance);
+    }
+  };
+}
+const account = createBankAccount();
+account.deposit(500);     
+account.withdraw(200);    
+account.withdraw(400);    
+console.log(account.balance); 
+account.checkBalance();  
