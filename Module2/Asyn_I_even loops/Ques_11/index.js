d50cb9221f20bb3718a8e665df8fcb16d12980b4
SowmisHub <vemulasowmika123@gmail.com>
2025-11-27 23:05:54 +0530

    ques11

---
+++ Module2/Asyn_I_even loops/Ques_11/index.js
@@ -0,0 +1,19 @@
+function makeCoffee() {
+  console.log("Making coffee...");
+}
+
+function readNewspaper() {
+  console.log("Reading the newspaper...");
+}
+
+function eatBreakfast() {
+  console.log("Eating breakfast...");
+}
+
+console.log("Starting my morning routine...");
+
+makeCoffee();
+readNewspaper();
+eatBreakfast();
+
+console.log("Morning routine completed.");
